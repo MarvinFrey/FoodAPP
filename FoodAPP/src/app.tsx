3d7ecf57057b1b1a8730c202dd09926c733c3efb
m.frey@digi-zeiterfassung.de <m.frey@digi-zeiterfassung.de>
2023-03-08 07:55:54 +0100

    Rearrange

--- FoodAPP/src/app.tsx
+++
@@ -1,9 +0,0 @@
-import { useState } from 'preact/hooks';
-
-export function App() {
-  return (
-    <div>
-      Hallo
-    </div>
-  );
-}
---
+++ src/app.tsx
@@ -0,0 +1,9 @@
+import FoodAPP from "./Components/FoodAPP";
+
+export function App() {
+  return (
+    <div>
+      <FoodAPP />
+    </div >
+  );
+}
