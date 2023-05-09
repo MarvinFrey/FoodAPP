4e5d4362a2eeb5cf9e3a9ddd8b5c6de2c781ad2a
m.frey@digi-zeiterfassung.de <m.frey@digi-zeiterfassung.de>
2023-05-09 08:45:56 +0200

    React Router hinzugefügt

--- FoodAPP/src/app.tsx
+++ FoodAPP/src/app.tsx
@@ -1,9 +1,27 @@
 import FoodAPP from "./Components/FoodAPP";
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <FoodAPP />,
+  },
+  {
+    path: "/about",
+    element: <div>Hallo</div>,
+  },
+]);
 
 export function App() {
   return (
     <div class="mx-auto w-80 text-center pt-5">
-      <FoodAPP />
+      <RouterProvider router={router} />
+
+
+
     </div >
   );
 }
